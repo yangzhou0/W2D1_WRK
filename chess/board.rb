15e2790af15e848190a7b1c00d5e12f5ce110392
Sam and Yang <rictorlome@gmail.com>
2018-01-22 16:35:24 -0500

    complete Phase1 Board

---
+++ chess/board.rb
@@ -0,0 +1,31 @@
+require_relative 'piece'
+
+class Board
+  def initialize
+    @grid = Array.new(8) { Array.new(8) {Piece.new} }
+  end
+
+  def [](pos)
+    x, y = pos
+    @grid[x][y]
+  end
+
+  def []=(pos,val)
+    x, y = pos
+    @grid[x][y] = val
+  end
+
+  def is_on_board? (pos)
+    self[pos].is_a? (Piece)
+  end
+
+  def move_piece(start_pos, end_pos)
+    piece = self[start_pos]
+    if !self.is_on_board?(start_pos) || piece.class == NullPiece
+      raise StandardError
+      # raise StandardError unless piece.can_move?(end_pos)
+    end
+    self[start_pos], self[end_pos] = NullPiece.new, piece
+  end
+
+end
---
+++ chess/piece.rb
@@ -0,0 +1,11 @@
+class Piece
+  def initialize
+    @atr = nil
+  end
+end
+
+class NullPiece < Piece
+  def initialize
+    @atr = nil
+  end
+end
